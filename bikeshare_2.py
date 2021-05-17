07df54dd7bfdb6d680e8908ef22b8b611a07756a
Dinesh Seyydri <dinesh_seyyadri@hotmail.com>
2021-05-17 18:08:22 +0100

    bikeshare_2.py modified to make comments user friendly

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -70,7 +70,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city and filters by month and day .
 
     Args:
         (str) city - name of the city to analyze
@@ -103,7 +103,7 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """ Displays statistics on the most frequent times of travel """
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
@@ -128,7 +128,7 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """ Displays statistics on the most popular stations and trip """
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
